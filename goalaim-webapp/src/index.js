ae8ea53586484abd09e7b68d147e5966e2dde2a4
DonatNathan <nathan.donat-filliod@epitech.eu>
2024-11-26 11:47:38 -0800

    [ADD] - Google analytics with ReactGA

--- goalaim-webapp/src/index.js
+++ goalaim-webapp/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ReactGA from "react-ga4";
+
+ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_KEY);
+ReactGA.send("pageview");
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
