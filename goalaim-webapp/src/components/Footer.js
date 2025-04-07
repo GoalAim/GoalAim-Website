dbb5d9e8b6135bdbea9c59ff5f05f7552631191c
DonatNathan <nathan.donat-filliod@epitech.eu>
2025-04-07 10:59:53 -0700

    [ADD] - Clickable logo and links in footer

--- goalaim-webapp/src/components/Footer.js
+++ goalaim-webapp/src/components/Footer.js
@@ -25,9 +25,18 @@ const Footer = () => {
         <Box style={{backgroundColor: theme.BackgroundColor, display: "flex", flexDirection: "column"}}>
             <Box style={{alignSelf: "center", width: "80%", border: "solid", borderWidth: 2, borderColor: theme.BorderColor, borderRadius: 100, marginTop: "100px", marginBottom: "100px"}}></Box>
             <Box style={{marginBottom: "50px", display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-around"}}>
-                <ul style={{display: "flex", flexDirection: "row", justifyContent: "space-between", listStyleType: "none"}}>
+                <ul style={{display: "flex", flexDirection: "row", justifyContent: "space-between", listStyleType: "none", width: "20%"}}>
+                    <ListElement>
+                        <FooterLink to="/" text="Home" />
+                    </ListElement>
                     <ListElement>
                         <FooterLink to="/privacy-policy" text="Privacy" />
+                    </ListElement>
+                    <ListElement>
+                        <FooterLink to="/support" text="Support" />
+                    </ListElement>
+                    <ListElement>
+                        <FooterLink to="/blog" text="Blog" />
                     </ListElement>
                 </ul>
                 <Typography style={{color: theme.TextColor, fontSize: 20}}>© 2025 - GoalAim</Typography>
