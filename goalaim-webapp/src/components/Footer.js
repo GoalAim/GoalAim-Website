939dd6874fc4dd2a20e84e0c1ec0f185b06c7e76
DonatNathan <nathan.donat-filliod@epitech.eu>
2025-04-16 11:12:16 -0700

    [FIX] - Responsive in navbar and footer

--- goalaim-webapp/src/components/Footer.js
+++ goalaim-webapp/src/components/Footer.js
@@ -1,12 +1,8 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ThemeContext } from "../utils/themes/ThemeContext";
 import Box from '@mui/material/Box';
 import { Typography } from "@mui/material";
 import { Link } from "react-router-dom";
-import styled from 'styled-components';
-
-const ListElement = styled.li`
-`
 
 const FooterLink = ({to, text}) => {
 
@@ -19,25 +15,32 @@ const FooterLink = ({to, text}) => {
 
 const Footer = () => {
 
+    const [width, setWidth] = useState(window.innerWidth);
     const {theme} = useContext(ThemeContext);
+
+    useEffect(() => {
+        const handleResize = () => setWidth(window.innerWidth);
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
 
     return (
         <Box style={{backgroundColor: theme.BackgroundColor, display: "flex", flexDirection: "column"}}>
             <Box style={{alignSelf: "center", width: "80%", border: "solid", borderWidth: 2, borderColor: theme.BorderColor, borderRadius: 100, marginTop: "100px", marginBottom: "100px"}}></Box>
-            <Box style={{marginBottom: "50px", display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-around"}}>
-                <ul style={{display: "flex", flexDirection: "row", justifyContent: "space-between", listStyleType: "none", width: "20%"}}>
-                    <ListElement>
+            <Box style={{marginBottom: "50px", display: "flex", flexDirection: width < 1024 ? "column" : "row", alignItems: "center", justifyContent: "space-around"}}>
+                <ul style={{display: "flex", flexDirection: "row", justifyContent: "center", listStyleType: "none", width: "20vw", marginRight: "35px"}}>
+                    <li style={{margin: "10px"}}>
                         <FooterLink to="/" text="Home" />
-                    </ListElement>
-                    <ListElement>
+                    </li>
+                    <li style={{margin: "10px"}}>
                         <FooterLink to="/privacy-policy" text="Privacy" />
-                    </ListElement>
-                    <ListElement>
+                    </li>
+                    <li style={{margin: "10px"}}>
                         <FooterLink to="/support" text="Support" />
-                    </ListElement>
-                    <ListElement>
+                    </li>
+                    <li style={{margin: "10px"}}>
                         <FooterLink to="/blog" text="Blog" />
-                    </ListElement>
+                    </li>
                 </ul>
                 <Typography style={{color: theme.TextColor, fontSize: 20}}>© 2025 - GoalAim</Typography>
             </Box>
